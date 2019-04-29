e64179ef2d5083c62da89f383cf4be7a3416c57e
Miguel Molinos Pérez <m.molinos@alumnos.upm.es>
2019-04-29 18:43:17 +0200

    Minor changes 29-4-2019

--- GIMP/src/main.c
+++ GIMP/src/main.c
@@ -31,8 +31,20 @@ void main(int argc, char *argv[])
   /* Read mesh data */
   ReadGidMesh(argv[1],&ElemProp,&MeshProp,&Dict);
 
-
-
+  for(int i = 0 ; i<MeshProp.Nnodes ; i++){
+    printf("%f ; %f ; %f \n",
+	   MeshProp.Coordinates[i][0],
+	   MeshProp.Coordinates[i][1],
+	   MeshProp.Coordinates[i][2]);
+  }
+  printf("\n");
+  for(int i = 0 ; i<MeshProp.Nelem ; i++){
+    for(int j = 0 ; j<ElemProp.Nnodes ; j++){
+      printf(" %i ",
+	     MeshProp.Connectivity[i][j]);
+    }
+    printf("\n");
+  }
 
   /* Read the mesh */
 
@@ -111,22 +123,22 @@ void main(int argc, char *argv[])
 
   K = Get_Stiffness_Matrix(&ElementMesh);
 
-  /* printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n", */
-  /* 	 K.nM[0][0],K.nM[0][1],K.nM[0][2],K.nM[0][3],K.nM[0][4],K.nM[0][5],K.nM[0][6],K.nM[0][7]); */
-  /* printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n", */
-  /* 	 K.nM[1][0],K.nM[1][1],K.nM[1][2],K.nM[1][3],K.nM[1][4],K.nM[1][5],K.nM[1][6],K.nM[1][7]); */
-  /* printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n", */
-  /* 	 K.nM[2][0],K.nM[2][1],K.nM[2][2],K.nM[2][3],K.nM[2][4],K.nM[2][5],K.nM[2][6],K.nM[2][7]); */
-  /* printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n", */
-  /* 	 K.nM[3][0],K.nM[3][1],K.nM[3][2],K.nM[3][3],K.nM[3][4],K.nM[3][5],K.nM[3][6],K.nM[3][7]); */
-  /* printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n", */
-  /* 	 K.nM[4][0],K.nM[4][1],K.nM[4][2],K.nM[4][3],K.nM[4][4],K.nM[4][5],K.nM[4][6],K.nM[4][7]); */
-  /* printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n", */
-  /* 	 K.nM[5][0],K.nM[5][1],K.nM[5][2],K.nM[5][3],K.nM[5][4],K.nM[5][5],K.nM[5][6],K.nM[5][7]); */
-  /* printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n", */
-  /* 	 K.nM[6][0],K.nM[6][1],K.nM[6][2],K.nM[6][3],K.nM[6][4],K.nM[6][5],K.nM[6][6],K.nM[6][7]); */
-  /* printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n", */
-  /* 	 K.nM[7][0],K.nM[7][1],K.nM[7][2],K.nM[7][3],K.nM[7][4],K.nM[7][5],K.nM[7][6],K.nM[7][7]); */
+  printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n",
+  	 K.nM[0][0],K.nM[0][1],K.nM[0][2],K.nM[0][3],K.nM[0][4],K.nM[0][5],K.nM[0][6],K.nM[0][7]);
+  printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n",
+  	 K.nM[1][0],K.nM[1][1],K.nM[1][2],K.nM[1][3],K.nM[1][4],K.nM[1][5],K.nM[1][6],K.nM[1][7]);
+  printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n",
+  	 K.nM[2][0],K.nM[2][1],K.nM[2][2],K.nM[2][3],K.nM[2][4],K.nM[2][5],K.nM[2][6],K.nM[2][7]);
+  printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n",
+  	 K.nM[3][0],K.nM[3][1],K.nM[3][2],K.nM[3][3],K.nM[3][4],K.nM[3][5],K.nM[3][6],K.nM[3][7]);
+  printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n",
+  	 K.nM[4][0],K.nM[4][1],K.nM[4][2],K.nM[4][3],K.nM[4][4],K.nM[4][5],K.nM[4][6],K.nM[4][7]);
+  printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n",
+  	 K.nM[5][0],K.nM[5][1],K.nM[5][2],K.nM[5][3],K.nM[5][4],K.nM[5][5],K.nM[5][6],K.nM[5][7]);
+  printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n",
+  	 K.nM[6][0],K.nM[6][1],K.nM[6][2],K.nM[6][3],K.nM[6][4],K.nM[6][5],K.nM[6][6],K.nM[6][7]);
+  printf("[%f ; %f ; %f ; %f ; %f ; %f ; %f ; %f ] \n",
+  	 K.nM[7][0],K.nM[7][1],K.nM[7][2],K.nM[7][3],K.nM[7][4],K.nM[7][5],K.nM[7][6],K.nM[7][7]);
 
   
   exit(EXIT_SUCCESS);
