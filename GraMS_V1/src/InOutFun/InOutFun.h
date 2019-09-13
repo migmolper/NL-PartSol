5d0fd113cc223245e88d58a5d266db7570527b09
Miguel Molinos <m.molinos@alumnos.upm.es>
2019-09-13 17:29:46 +0200

    Minor changes 13-9-2019

--- GraMS_V1/src/InOutFun/InOutFun.h
+++ GraMS_V1/src/InOutFun/InOutFun.h
@@ -9,25 +9,32 @@
 /* Word Parser */
 enum { MAXW = 100, MAXC = 1000 };
 
-/* Auxiliar functions */
-Curve ReadCurve(char *);
+/* GnuPlotOutput.c */
+void WriteGnuplot(Matrix, Matrix, double, double, int, int, char[20]);
+
+/* Parse.c */
 int parse(char **words, char *str, char *delims);
 
-/* Inputs */
-Mesh ReadGidMesh(char *);
+/* ReadCSV.c */
 Matrix Read_CSV(char *, int);
 
-/* Read parameters from the .dat file */
+/* ReadCurve.c */
+Curve ReadCurve(char *);
+
+/* ReadDatFile.c */
 void Read_GeneralParameters(char *);
 Boundaries Set_FEM_BCC(char *, Mesh);
-void Read_MPM_InitVal(char *, GaussPoint);
 LoadCase Read_MPM_LoadCase_ExtForces(char *,GaussPoint);
 LoadCase Read_MPM_LoadCase_BodyForces(char *,GaussPoint);
+void Read_MPM_InitVal(char *, GaussPoint);
 
-/* Outputs */
-void WriteGnuplot(Matrix, Matrix, double, double, int, int, char[20]);
+/* ReadGidMesh.c */
+Mesh ReadGidMesh(char *);
+
+/* WriteVtk.c */
 void WriteVtk_MPM(char *, GaussPoint, Matrix, int);
 void WriteVtk_FEM(char *, Mesh, Matrix, int);
 void WriteVtk_Float_Scalar(char *, Matrix);
 void WriteVtk_Float_Vector(char *, Matrix);
 void WriteVtk_Float_Tensor(char *, Matrix);
+
