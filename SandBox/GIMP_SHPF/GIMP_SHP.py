7ede877d99625eed2360a44d2707c5b1109d3980
Miguel Molinos <m.molinos@alumnos.upm.es>
2019-11-15 17:27:15 +0100

    Minor changes 15-11-2019

--- SandBox/GIMP_SHPF/GIMP_SHP.py
+++ SandBox/GIMP_SHPF/GIMP_SHP.py
@@ -38,7 +38,32 @@ def d_GIMP(L, lp, Xp, Xi):
         d_Sip = -(1/(2*L*lp))*(L+lp-Xp+Xi)
     return d_Sip
 
-def print_d_GIMP():
+
+def print_dx_GIMP():
+    N = 50
+    xp = yp = np.linspace(-1.5, 1.5, N)
+    Xp, Yp = np.meshgrid(xp, yp, indexing='ij', sparse=False)
+    Sip_xDy = np.zeros([N,N])
+    xi = yi = 0
+    L = 1.
+    lp = 1/4.
+
+    for i in range(0,N):
+        for j in range(0,N):
+            Sip_xDy[i][j] = d_GIMP(L,lp,xp[i],xi) * GIMP(L,lp,yp[j],yi) 
+            
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(Xp, Yp, Sip_xDy, rstride=1, cstride=1, cmap=cm.jet,
+                           linewidth=0, antialiased=False)
+    ax.set_zlim3d(-1.00, 1.00)
+
+    # ax.w_zaxis.set_major_locator(LinearLocator(10))
+    # ax.w_zaxis.set_major_formatter(FormatStrFormatter('%.03f'))
+    
+    plt.show()
+
+def print_dy_GIMP():
     N = 50
     xp = yp = np.linspace(-1.5, 1.5, N)
     Xp, Yp = np.meshgrid(xp, yp, indexing='ij', sparse=False)
@@ -109,5 +134,6 @@ def Cos1D_GIMP():
     plt.show()
 
 
-print_d_GIMP()
+print_dy_GIMP()
+print_dx_GIMP()
 print_GIMP()
