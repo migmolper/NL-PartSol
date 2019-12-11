59a4df66388a41ca68e99f37ec03223752cbc2d8
Miguel Molinos <m.molinos@alumnos.upm.es>
2019-12-11 21:20:20 +0100

    Minor changes V 11-12-2019
    Deformation energy implemented and tested

--- GraMS_V1/src/InOutFun/WriteVtk.c
+++ GraMS_V1/src/InOutFun/WriteVtk.c
@@ -75,8 +75,13 @@ void WriteVtk_MPM(char * Name_File, GaussPoint MPM_Mesh,
   fprintf(Vtk_file,"SCALARS DENSITY float \n");
   fprintf(Vtk_file,"LOOKUP_TABLE default \n");
   for(int i =  0 ; i<MPM_Mesh.NumGP ; i++){
-    fprintf(Vtk_file,"%f \n",MPM_Mesh.Phi.rho.nV[i]);
-    
+    fprintf(Vtk_file,"%f \n",MPM_Mesh.Phi.rho.nV[i]); 
+  }
+
+  fprintf(Vtk_file,"SCALARS W float \n");
+  fprintf(Vtk_file,"LOOKUP_TABLE default \n");
+  for(int i =  0 ; i<MPM_Mesh.NumGP ; i++){
+    fprintf(Vtk_file,"%f \n",MPM_Mesh.Phi.W.nV[i]); 
   }
 
   /* float -> integer */
