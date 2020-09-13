cf2b48f698c9c49d653607882993647c4bdedf26
Miguel <m.molinos@outlook.es>
2020-09-13 19:30:06 +0200

    Excesive RAM consume II

--- nl-partsol/src/Solvers/U-Discrete-Energy-Momentum.c
+++ nl-partsol/src/Solvers/U-Discrete-Energy-Momentum.c
@@ -1557,7 +1557,8 @@ static void update_Particles(Matrix D_Displacement,
 	Free memory
        */
       free(Nodes_p.Connectivity);
-      free__MatrixLib__(ShapeFunction_p);
+      Free__MatrixLib__(ShapeFunction_p);
+      Free__MatrixLib__(gradient_p);
       free__TensorLib__(f_n1_p);
     }  
 }
