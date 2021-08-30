89f71a975bb54f92a6fa2e01d36fa6f61f393750
Miguel <m.molinos@outlook.es>
2021-08-30 10:46:12 +0200

    Print solver stats

--- nl-partsol/src/InOutFun/print_ScreenMessage.c
+++ nl-partsol/src/InOutFun/print_ScreenMessage.c
@@ -35,6 +35,19 @@ void print_convergence_stats(int Time, int Iter, double Error0, double Error_tot
       printf("\t Total error : %1.4e \n",Error_total);
       printf("\t Relative error : %1.4e \n",Error_relative);
     }
+
+    FILE * Stats_Solver;
+    char Name_file_t[80];
+    sprintf(Name_file_t,"%s/Stats_Solver.csv",OutputDir);  
+    Stats_Solver = fopen(Name_file_t,"a");
+
+    if(Time == 0)
+    {
+      fprintf(Stats_Solver,"%s,%s,%s\n","Iter","Total Error","Relative Error");
+    }
+    fprintf(Stats_Solver,"%i,%1.4e,%1.4e\n",Iter,Error_total,Error_relative);
+
+    fclose(Stats_Solver);
 }
 
 /*********************************************************************/
