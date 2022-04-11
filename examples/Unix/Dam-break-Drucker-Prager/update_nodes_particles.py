bed1a4008eff7ae05cdd6cd57f0114cc8c06c7fe
Miguel <m.molinos@alumnos.upm.es>
2022-04-11 11:58:59 +0200

    Create new test

---
+++ examples/Unix/Dam-break-Drucker-Prager/update_nodes_particles.py
@@ -0,0 +1,9 @@
+import pandas as pd                                                                                                                                                                           
+
+files = ("Walls_x_container","Walls_y_container","Walls_x","Walls_y","Soil")
+
+for i in files: 
+    file_gid = i+"_GID.txt" 
+    file_def = i+".txt" 
+    data = pd.read_csv(file_gid,header=None,sep='\s+',engine='python') 
+    (data[0]-1).to_csv(file_def,header=None,index=False) 
