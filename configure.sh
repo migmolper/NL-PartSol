6d4a1dc2099b98f282b5c850ce9d2dcb621ba4c7
Miguel Molinos <m.molinos@outlook.es>
2022-10-23 11:50:28 +0200

    Minor changes

--- configure.sh
+++ configure.sh
@@ -25,7 +25,7 @@ if [[ "$HOSTNAME" == "Hilbert" ]]
  export PETSC_DIR=$HOME/petsc
  export PETSC_ARCH=arch-linux-c-debug
  export PKG_CONFIG_PATH=$PETSC_DIR/$PETSC_ARCH/lib/pkgconfig
- C_COMPILER=$PETSC_DIR/$PETSC_ARCH/bin/mpicc
+ C_COMPILER=/usr/local/openmpi-4.1.2/bin/mpicc
 
 elif [[ "$HOSTNAME" == "MacBook-Pro-de-Miguel.local" ]]
  then
